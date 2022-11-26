be2a9dfe16710607dc93f54b893c161f21555088
Lusuarezb <lusuarezb@unal.edu.co>
2022-11-25 20:48:21 -0500

    Intento 2

--- pregunta.py
+++ pregunta.py
@@ -19,8 +19,9 @@ def limpiar_fecha(linea):
 
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df.drop(df.columns[0], inplace=True, axis=1)
 
-    df.drop_duplicates(inplace=True) # Eliminar duplicados
+    #df.drop_duplicates(inplace=True) # Eliminar duplicados
     df.dropna(inplace=True) # Eliminar vacios
 
     # Minusculas
@@ -28,6 +29,7 @@ def clean_data():
     df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
     df['idea_negocio'] = df['idea_negocio'].str.lower() 
     df['barrio'] = df['barrio'].str.lower() 
+    df['línea_credito'] = df['línea_credito'].str.lower()
 
     # Limpieza de strings
     df.idea_negocio = df.idea_negocio.str.replace('-',' ')
@@ -36,17 +38,19 @@ def clean_data():
 
     df.barrio = df.barrio.str.replace('-',' ')
     df.barrio = df.barrio.str.replace('_',' ')
-    df.barrio = df.barrio.str.strip()
+    #df.barrio = df.barrio.str.strip()
 
     df.línea_credito = df.línea_credito.str.replace('-',' ')
     df.línea_credito = df.línea_credito.str.replace('_',' ')
-    
+    df.línea_credito = df.línea_credito.str.strip()
 
     # Tipos
     df.comuna_ciudadano = df.comuna_ciudadano.astype(float)
 
     # Fecha
     df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(limpiar_fecha)
+    df['fecha_de_beneficio'] = pd.to_datetime(df.fecha_de_beneficio, format='%Y/%m/%d')
+    # df.fecha_de_beneficio = df.fecha_de_beneficio.apply(lambda x : pd.to_datetime(x, format="%Y/%m/%d") if re.match('^\d{4}/', x) else pd.to_datetime(x, format="%d/%m/%Y"))
 
     # # Monto
     df.monto_del_credito = df.monto_del_credito.str.replace('$',"")
@@ -56,6 +60,7 @@ def clean_data():
     df.monto_del_credito = df.monto_del_credito.astype(int)
 
     # Final
-    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True) # Eliminar vacios
+    df.drop_duplicates(inplace=True) # Eliminar duplicados
 
     return df
